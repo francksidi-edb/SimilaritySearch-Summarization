f7df3a5a30a0c8a34e59ff42b4139c7739eb6e7a
francksidi-edb <164478695+francksidi-edb@users.noreply.github.com>
2024-03-27 13:15:36 -0400

    Update utilities.sql

--- ddl/utilities.sql
+++ ddl/utilities.sql
@@ -16,3 +16,13 @@ RETURNS text AS $$
 import pkg_resources
 return "\n".join([f"{i.key}=={i.version}" for i in pkg_resources.working_set])
 $$ LANGUAGE plpython3u;
+
+CREATE OR REPLACE FUNCTION py_add_path()
+RETURNS text AS $$
+import sys
+# Path to the site-packages directory
+new_path = '/Users/ton.machielsen/.local/lib/python3.11/site-packages'
+if new_path not in sys.path:
+    sys.path.append(new_path)
+return str(sys.path)
+$$ LANGUAGE plpython3u;
